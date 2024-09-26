4dc5e14dcc537f7f8c2a107231c74465c9c291cb
Alina Konkova <99170615+AlinaKonkova@users.noreply.github.com>
2024-09-26 23:42:18 +0300

    bc

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # или укажите конкретные домены, напр., ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы (GET, POST, OPTIONS и т.д.)
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
+
+class Data(BaseModel):
+    data: str
+
+@app.post("/api/data")
+async def receive_data(data: Data):
+    with open('data.txt', 'a') as file:
+        file.write(f'{data.data}\n')
+    return {"message": "Данные получены"}
